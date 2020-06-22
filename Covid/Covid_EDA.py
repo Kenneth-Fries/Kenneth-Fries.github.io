3960db99dc4d8663b95f1af3c554aae93635323e
Kenneth Fries <Kenneth_Fries@Yahoo.com>
2020-06-22 14:22:41 -0700

    added Covid web page

---
+++ Covid/Covid_EDA.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # This is to be a project on covid 19.  
+
+
+import pandas as pd
+from matplotlib import pyplot as plt
+from pprint import pprint
+import numpy
+import json
+
+
+
+
+url_deaths = "https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv"
+url_confirmed = "https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
+
+
+
+df_deaths = pd.read_csv(url_deaths)
+df_confirmed = pd.read_csv(url_confirmed)
+
+
+
+df_deaths.fillna(0, inplace=True)
+df_confirmed.fillna(0, inplace=True)
+
+
+
+
+covid_cases = {}                                  #make dictionary for all data
+countries = df_deaths.Country_Region.unique()     #get list of countries
+
+for country in countries:                         #If multiple countries
+    states = df_deaths.Province_State.unique()
+    
+    covid_cases.update({country:{}})              #Add the new country name and create a sub-dict
+    
+    for state in states:
+        state_df = df_deaths[df_deaths.Province_State == state]
+        counties = state_df.Admin2.to_list()
+        covid_cases[country].update({state:{}})
+        
+        for county in counties:
+            df = state_df[state_df.Admin2 == county].T[12:]    #columns start at 12 with one datapoint per column
+                                                               #Transpose date columns to row index
+            #df.index = pd.to_datetime(df.index)                #Set row index to datetime
+            county_data = df.diff(1).fillna(0)                 #Find delta
+            
+            county_smoothed_7day = df.diff(7).fillna(0) / 7    #Find 7 day avg delta
+            
+            
+            
+            try: 
+                iso2 = state_df.iso2[state_df.Admin2 == county].values[0]    #State Abbreviation
+                population = state_df.Population[state_df.Admin2 == county].values[0]   # population
+                covid_cases[country][state].update({
+                                                county:{
+                                                    "lat" : str(state_df[state_df.Admin2 == county].Lat.values[0]),
+                                                    "long" : str(state_df[state_df.Admin2 == county].Long_.values[0]),
+                                                    "deaths" : [],
+                                                    "deaths_7_day" : [],
+                                                    "dates" : [],
+                                                    "abbreviation": iso2,
+                                                    "population": str(population)
+                                            }})
+                for m, y in enumerate(county_data):
+                    for x in list(zip(county_data[y].index, county_data[y].values)):
+                        covid_cases[country][state][county]['deaths'].append(int(x[1]))
+                        covid_cases[country][state][county]['dates'].append(x[0])
+                        
+                for m, y in enumerate(county_smoothed_7day):
+                    for x in list(zip(county_smoothed_7day[y].index, county_smoothed_7day[y].values)):
+                        if covid_cases[country][state][county]['dates'][m] == x[0]:
+                            covid_cases[country][state][county]['deaths_7_day'].append(int(x[1]))
+                        
+                                                                            
+        
+
+                
+            
+            except:
+                
+                print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  Didn't work here !!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+                print(state, county)
+                covid_cases[country][state].update({
+                                                county:{
+                                                    "lat" : "Not Available",
+                                                    "long" : "Not Available",
+                                                    #"deaths" : county_data,
+                                                    #"deaths_7_day" : county_smoothed_7day,
+                                                    "abbreviation": "Not Available",
+                                                    "population": "Not Available"
+                                            }})
+                
+            
+
+        
+    
+
+
+
+countries = df_confirmed.Country_Region.unique()
+for country in countries:
+    print(country, "{")
+    states = df_confirmed.Province_State.unique()
+    
+    for n, state  in enumerate(states):
+        
+        if n < 60:      # Useful for allowing only a few states to run.
+            state_df = df_confirmed[df_confirmed.Province_State == state]
+            counties = state_df.Admin2.to_list()
+            print("    ", state, " #", len(counties), " counties.")
+            for county in counties:
+                df = state_df[state_df.Admin2 == county].T[11:]    #columns start at 12 with one datapoint per column
+                                                                   #Transpose date columns to row index
+                #df.index = pd.to_datetime(df.index)                #Set row index to datetime
+                county_data = df.diff(1).fillna(0)                 #Find delta
+                county_smoothed_7day = df.diff(7).fillna(0) / 7    #Find 7 day avg delta
+                iso2 = state_df.iso2[state_df.Admin2 == county]
+                
+                covid_cases[country][state][county].update({
+                        "confirmed" : [],
+                        "confirmed_7_day" : [],
+                        "county_name_check" : county
+                        })
+                for m, y in enumerate(county_data):
+                    for x in list(zip(county_data[y].index, county_data[y].values)):
+                        covid_cases[country][state][county]['confirmed'].append(int(x[1]))    
+                        
+                for m, y in enumerate(county_smoothed_7day):
+                    for x in list(zip(county_smoothed_7day[y].index, county_smoothed_7day[y].values)):
+                        covid_cases[country][state][county]['confirmed_7_day'].append(int(x[1]))    
+                
+ 
+
+
+with open('data.json', 'w') as fp:
+    json.dump(covid_cases, fp)
+
+print("Finished " )
---
+++ Covid/plot.js
@@ -0,0 +1,252 @@
+// Prevent the page from refreshing
+//d3.event.preventDefault();
+
+
+
+
+var country = "US";
+var state = "";
+var county = "";
+var url = `data.json`;
+var xArr = [];
+var yArr = [];
+var allMyData = {};
+var states = [];
+var dataChoice = "";
+var dataSetChoice = "";
+
+function getData() {
+    d3.json(url).then(function(data) {
+        allMyData = data;
+        console.log(allMyData);
+        drawStateSelect(); //data must be fully loaded before this.
+    });
+
+
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+// draw state select
+
+function drawStateSelect() {
+    // start with clear page:
+    document.getElementById("selectBoxes").innerHTML = "";
+
+    //get data
+    states = Object.keys(allMyData.US);
+
+    //draw select dropdown
+    var sel = d3.select("#selectBoxes")
+        .append("select")
+        .text("pick a state")
+        .attr("id", "stateSelect")
+        .attr("onchange", "drawCountySelect(this.value)");
+
+    //draw default
+    var defaultOption = d3.select("#stateSelect")
+        .append("option")
+        .text("pick a state")
+        .attr("value", "");
+
+    var stateSelect = document.getElementById('stateSelect');
+    for (var i = 0; i < states.length; i++) {
+        var opt = document.createElement('option');
+        opt.innerHTML = states[i];
+        opt.value = states[i];
+        stateSelect.appendChild(opt);
+    }
+
+
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////
+
+
+
+function drawCountySelect(theState) {
+    //get data
+    state = theState;
+    console.log(state);
+    console.log(Object.keys(allMyData.US[state]))
+
+    if (theState) {
+        //clear plot
+        if (document.getElementById('plot')) {
+            document.getElementById('plot').remove()
+        };
+        if (document.getElementById('dataSelect')) {
+            document.getElementById('dataSelect').remove()
+        };
+        if (document.getElementById('countySelect')) {
+            document.getElementById('countySelect').remove()
+        };
+
+        //create county select tag
+        if (!document.getElementById('countySelect')) {
+            var countySelect = d3.select("#selectBoxes")
+                .append("select")
+                .text("pick a county")
+                .attr("id", "countySelect")
+                .attr("onchange", "drawDataSelect(this.value)");
+        }
+        //console.log(allMyData.US[state]);
+        counties = Object.keys(allMyData.US[state]);
+
+        var countySelect = document.getElementById('countySelect');
+        //clear any old data
+        countySelect.innerHTML = "";
+
+        //draw default
+        var defaultOption = d3.select("#countySelect")
+            .append("option")
+            .text("pick a county")
+            .attr("value", "");
+
+        for (var i = 0; i < counties.length; i++) {
+            var opt = document.createElement('option');
+            opt.innerHTML = counties[i];
+            opt.value = counties[i];
+            countySelect.appendChild(opt);
+        }
+
+        //county = document.getElementById('countySelect').nodeValue
+
+
+    } else {
+        document.getElementById('countySelect').remove();
+        document.getElementById('dataSelect').remove();
+        document.getElementById('plot').remove();
+
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////
+
+
+
+function drawDataSelect(theCounty) {
+    //get data
+    county = theCounty;
+    console.log("drawDataSelect keys for " + county + ' ' + state);
+    console.log(Object.keys(allMyData.US[state][county]))
+
+    if (county) {
+        //clear plot
+        if (document.getElementById('plot')) {
+            document.getElementById('plot').remove()
+        };
+
+        //create county select tag
+        if (!document.getElementById('dataSelect')) {
+            var dataSelect = d3.select("#selectBoxes")
+                .append("select")
+                .text("pick a dataset")
+                .attr("id", "dataSelect")
+                .attr("onchange", "buildPlot(this.value)");
+        }
+
+        //
+        dataChoices = Object.keys(allMyData.US[state][county]);
+
+        var dataSelect = document.getElementById('dataSelect');
+
+        //clear any old data
+        dataSelect.innerHTML = "";
+
+        //draw default
+        var defaultOption = d3.select("#dataSelect")
+            .append("option")
+            .text("pick a dataset")
+            .attr("value", "");
+
+        //draw options
+        for (var i = 0; i < dataChoices.length; i++) {
+            console.log(dataChoices[i].toString())
+            if (["deaths", "deaths_7_day", "confirmed", "confirmed_7_day"].includes(dataChoices[i])) {
+                var opt = document.createElement('option');
+                opt.innerHTML = dataChoices[i];
+                opt.value = dataChoices[i];
+                dataSelect.appendChild(opt)
+            }
+        }
+
+
+
+    } else {
+        document.getElementById('dataSelect').remove();
+        document.getElementById('plot').remove();
+
+    }
+}
+
+//////////////////////////////////////////////////////////////
+
+function buildPlot(dataChoice) {
+    // 
+
+    dataSetChoice = dataChoice;
+
+    d3.select("body")
+        .append("div")
+        .attr("id", "plot");
+
+    county = document.getElementById('countySelect').value
+    var name = county + ' ' + state + ' ' + dataSetChoice;
+    console.log(state)
+    console.log(county)
+    console.log(Object.keys(allMyData.US[state][county]))
+        //console.log(allMyData.US[state][county].dates.map(d3.timeParse("%-m/%-d/%y")));
+    var dates = allMyData.US[state][county].dates.map(d3.timeParse("%-m/%-d/%y"));
+    var plotData = allMyData.US[state][county][dataSetChoice];
+
+    var trace1 = {
+        type: "scatter",
+        mode: "lines",
+        name: name,
+        x: dates,
+        y: plotData,
+        line: {
+            color: "#17BECF"
+        }
+    };
+
+    var data = [trace1];
+
+    var layout = {
+        title: `${name}`,
+        xaxis: {
+            autorange: true,
+
+            range: d3.extent(trace1.x),
+            type: "date"
+        },
+        yaxis: {
+            autorange: true,
+            type: "linear"
+        }
+    };
+
+    Plotly.newPlot("plot", data, layout);
+
+    /* 
+
+        // Add event listener for submit button
+        //d3.select("#submit").on("click", handleSubmit);
+
+        d3.selectAll("selDataset").on("change", updateCounties);
+     */
+
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+
+getData();
